2e577ef2f88be82c1f341294055e8daedce6ed53
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-07-27 10:20:36 -0400

    added route connections between files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project BookAPI
+
+BookAPI 
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const bookRoutes = require('./controllers/books')
+
+const app = express()
+
+app.use('/books', bookRoutes)
+
+// db connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
+
+const PORT = process.env.PORT
+
+app.listen(PORT, console.log(`listening on port ${PORT}`))
