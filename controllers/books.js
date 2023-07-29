fb545ba884822feb6b3920889b09bf19bbe368ba
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-07-29 09:42:00 -0400

    route posts

--- controllers/books.js
+++ controllers/books.js
@@ -1,6 +1,43 @@
 const router = require('express').Router()
 const Book = require('../models/books')
 
+router.get('/seed', (req, res) => {
+  Book.insertMany([{
+      "title": "The Shinobi Initiative",
+      "description": "The reality-bending adventures of a clandestine service agency in the year 2166",
+      "year": 2014,
+      "quantity": 10,
+      "imageURL": "https://imgur.com/LEqsHy5.jpeg"
+    },
+    {
+      "title": "Tess the Wonder Dog",
+      "description": "The tale of a dog who gets super powers",
+      "year": 2007,
+      "quantity": 3,
+      "imageURL": "https://imgur.com/cEJmGKV.jpg"
+    },
+    {
+      "title": "The Annals of Arathrae",
+      "description": "This anthology tells the intertwined narratives of six fairy tales.",
+      "year": 2016,
+      "quantity": 8,
+      "imageURL": "https://imgur.com/VGyUtrr.jpeg"
+    },
+    {
+      "title": "Wâˆ€RP",
+      "description": "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
+      "year": 2010,
+      "quantity": 4,
+      "imageURL": "https://imgur.com/qYLKtPH.jpeg"
+    }])
+      .then(res.status(200).json({
+          message: 'Seed successful'
+      }))
+      .catch(res.status(400).json({
+          message: 'Seed unsuccessful'
+      }))
+})
+
 router.get("/", (req, res) => {
     Book.find()
       .then((books) => {
@@ -27,42 +64,22 @@ router.get("/", (req, res) => {
       })
   })
   
- router.get('/seed', (req, res) => {
-    Book.insertMany([{
-        "title": "The Shinobi Initiative",
-        "description": "The reality-bending adventures of a clandestine service agency in the year 2166",
-        "year": 2014,
-        "quantity": 10,
-        "imageURL": "https://imgur.com/LEqsHy5.jpeg"
-      },
-      {
-        "title": "Tess the Wonder Dog",
-        "description": "The tale of a dog who gets super powers",
-        "year": 2007,
-        "quantity": 3,
-        "imageURL": "https://imgur.com/cEJmGKV.jpg"
-      },
-      {
-        "title": "The Annals of Arathrae",
-        "description": "This anthology tells the intertwined narratives of six fairy tales.",
-        "year": 2016,
-        "quantity": 8,
-        "imageURL": "https://imgur.com/VGyUtrr.jpeg"
-      },
-      {
-        "title": "Wâˆ€RP",
-        "description": "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
-        "year": 2010,
-        "quantity": 4,
-        "imageURL": "https://imgur.com/qYLKtPH.jpeg"
-      }])
-        .then(res.status(200).json({
-            message: 'Seed successful'
-        }))
-        .catch(res.status(400).json({
-            message: 'Seed unsuccessful'
-        }))
-})
+  router.put("/:id", async (req, res) => {
+    const { id } = req.params
+    await Book.findByIdAndUpdate(id, req.body)
+    res.status(303).redirect(`/books/${id}`)
+  })
+  
+  router.delete("/:id", async (req, res) => {
+    const { id } = req.params
+    await Book.findByIdAndDelete(id)
+    res.status(303).redirect('/books')
+  })
+  
+  router.post("/", async (req, res) => {
+    await Book.create(req.body)
+    res.status(303).redirect("/books")
+  })
 
 
 
